345e87510ff2b5837a77db769b9ccba0aaabbfa8
jerry <309032663@qq.com>
2019-04-18 17:13:40 +0800

    为了通过docker访问，修改mongodb访问地址为：192.168.0.1。(需提前执行：docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 localNet) 如果不通过docker，可以将地址修改回127.0.0.1。

--- src/com/cxyzy/ktor/demo/Application.kt
+++ src/com/cxyzy/ktor/demo/Application.kt
@@ -65,5 +65,5 @@ fun Application.module(testing: Boolean = false) {
 }
 
 val module = module {
-    single { KMongo.createClient("mongodb://127.0.0.1:27017").coroutine }
+    single { KMongo.createClient("mongodb://192.168.0.1:27017").coroutine }
 }
